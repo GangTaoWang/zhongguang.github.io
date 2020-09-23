20448f45a40a044eb5c2cbb201dcab381ea52706
weidongzhao3 <39546715+weidongzhao3@users.noreply.github.com>
2020-09-23 10:42:01 +0800

    修改

--- js/rem_main.js
+++ js/rem_main.js
@@ -1,12 +1,12 @@
-(function (doc, win) {
+(function(doc, win) {
     var docEl = doc.documentElement;
     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
+        recalc = function() {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
-            if(clientWidth>=750){
+            if (clientWidth >= 750) {
                 docEl.style.fontSize = '100px';
-            }else{
+            } else {
                 docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
             }
         };
